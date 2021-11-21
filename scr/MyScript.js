0a58c6494a38519af3172c70e9918adcff646be2
WetDumplings <44629862+WetDumplings@users.noreply.github.com>
2021-11-21 16:23:26 +0800

    optimize4

--- scr/MyScript.js
+++ scr/MyScript.js
@@ -33,7 +33,6 @@ function PageAnimation() {
     setTimeout(PageAnimation, txtspeed);
   }
   else{
-    Hellotxt.innerHTML = ("Width: "+ window.innerWidth.toString() + " Height: "+ window.innerHeight.toString())
     SE.style.transform = `translate(0px,0vh)`;
     SE.style.opacity = "1";
     resume.style.transform = `translate(0px,0vh)`;
